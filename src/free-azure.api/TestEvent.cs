885c1a2e104300bc0b79559818d2569fb0270c19
paule96 <paul-jeschke@outlook.com>
2022-06-11 18:57:37 +0000

    lets the test event really work

--- src/free-azure.api/TestEvent.cs
+++ src/free-azure.api/TestEvent.cs
@@ -17,10 +17,12 @@ namespace free_azure.api
     public class TestEvent
     {
         private readonly FreeAzureContext dbContext;
+        private readonly ILogger<TestEvent> logger;
 
-        public TestEvent(FreeAzureContext dbContext)
+        public TestEvent(FreeAzureContext dbContext, ILogger<TestEvent> logger)
         {
             this.dbContext = dbContext;
+            this.logger = logger;
         }
         [FunctionName("TestEvent")]
         public async Task<IActionResult> Run(
@@ -32,73 +34,33 @@ namespace free_azure.api
                 var postEvent = await JsonSerializer.DeserializeAsync<Event>(req.Body);
                 if (postEvent.Locations.Any(l => l.Virtual == false))
                 {
-                    /*
-                        SELECT
-    Count(1) as Count
-    -- e["Start"] <= '2022-06-11T19:01:55.0+00:00' as greaterOrEqualStart,
-    -- e["End"] >= '2022-06-11T20:01:55.0+00:00' as lessOrEqualEnd,
-    -- (
-    --     Select Count(1) as Count
-    --     From locations in e["Locations"]
-    --     Where
-    --         locations["Virtual"] = false And
-    --         Array_Contains(["test", "test2"], locations["Name"]) = false
-    -- )["Count"] as locationCount,
-    -- e as object
-FROM Events as e
-Where
-    e["Start"] <= '2022-06-11T19:01:55.0+00:00' And
-    e["End"] >= '2022-06-11T21:01:55.0+00:00' And
-    (
-        Select Count(1) as Count
-        From locations in e["Locations"]
-        Where
-            locations["Virtual"] = false And
-            Array_Contains(["test", "test2", "Super cool"], locations["Name"]) = true
-    )["Count"] > 0
-                    */
-                    // var query = new QueryDefinition(@$"
-                    //     Select
-                    //     From Events as
-                    //     Where
-                    //         Start >= '{postEvent.Start}' AND
-                    //         End <= '{postEvent.End}' AND
-                    //         ARRAY_CONTAINS(Locations,)
-
-                    // ");
-                    // var cosmosClient = dbContext.Database.GetCosmosClient();
-                    // var container = cosmosClient.GetDatabaseQueryIterator<Event>("");
-                    // var realLocationIds = postEvent.Locations
-                    //     .Where(l => l.Virtual == false)
-                    //     .Select(l => l.Id)
-                    //     .ToList();
                     var query = this.dbContext.Events.FromSqlRaw(@$"
                     SELECT
                         *
                     FROM Events as e
                     Where
-                        e[""Start""] <= '2022-06-11T19:01:55.0+00:00' And
-                        e[""End""] >= '2022-06-11T21:01:55.0+00:00' And
+                        (
+                            (
+                                -- the start of our event is in an exsisting event
+                                e[""Start""] <= '{postEvent.Start.ToString("yyyy-MM-ddTHH:mm:ss.fffffffZ")}' AND
+                                e[""End""] >= '{postEvent.Start.ToString("yyyy-MM-ddTHH:mm:ss.fffffffZ")}'
+                            ) OR
+                            (
+                                -- our end is to late, so we are overlapping with an other event
+                                e[""Start""] >= '{postEvent.Start.ToString("yyyy-MM-ddTHH:mm:ss.fffffffZ")}' AND
+                                e[""Start""] <= '{postEvent.End.ToString("yyyy-MM-ddTHH:mm:ss.fffffffZ")}'
+                            )
+                        ) AND
                         (
                             Select Count(1) as Count
                             From locations in e[""Locations""]
                             Where
                                 locations[""Virtual""] = false And
-                                Array_Contains([""test"", ""test2"", ""Super cool""], locations[""Name""]) = true
+                                Array_Contains([""{string.Join("\",\"", postEvent.Locations.Where(l => l.Virtual == false).Select(e => e.Name))}""], locations[""Name""]) = true
                         )[""Count""] > 0
                     ");
-                    // var query = this.dbContext.Events
-                    //     .Where(e =>
-                    //         e.Start >= postEvent.Start &&
-                    //         e.End <= postEvent.End
-                    //         e.Locations
-                    //             .Where(l => l.Virtual == false)
-                    //             .Count() > 0
-                    //             .Any(l =>
-                    //                 postEvent.Locations.Select(pl => pl.Name)
-                    //                     .Contains(l.Name)
-                    //             )
-                    //     );
+
+
                     var alreadyBooked = await query
                         .LongCountAsync() > 0;
                     if (alreadyBooked)
